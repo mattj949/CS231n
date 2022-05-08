9b2768d6d506bdae9f01530047a59c443d106c08
Annette Jing <annette.jing@gmail.com>
2022-05-07 21:25:03 -0700

    regression baseline for vix prediction

--- Utils.py
+++ Utils.py
@@ -6,6 +6,8 @@ import statsmodels as sm
 import matplotlib as plot
 import itertools
 from tqdm import tqdm
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 
 def convert_to_daily_returns(closing_prices):
     return closing_prices.pct_change()
@@ -26,5 +28,83 @@ def calc_cov(daily_rets, center_mass):
 def calc_sharpe(rets):
     return (rets.mean()[0] / rets.std()[0]) * np.sqrt(252)
 
+
 def scale_to_vol(rets, target_vol=.1):
     return rets * (target_vol / rets.std())
+
+
+def load_data(surface_path, target_path):
+    targets = pd.read_csv(target_path, skiprows=2, parse_dates=True, index_col=0)
+    targets = pd.DataFrame(targets['Close'])
+
+    surfaces = pd.read_csv(surface_path, parse_dates=True, index_col=[0,1])
+
+    dates = surfaces.index.get_level_values(0).unique()
+    dates = pd.Series(list(set(targets.index).intersection(set(dates))))
+    dates = dates.sort_values()
+
+    targets = targets.loc[dates]
+    surfaces = surfaces.loc[dates]
+    return surfaces, targets
+
+
+def target_volatility(surfaces, targets):
+    rets = convert_to_daily_returns(targets)
+    volatility = rets.rolling(window=252).std()*np.sqrt(252)
+    dates = volatility.dropna().index
+    return surfaces.loc[dates], volatility.dropna()
+
+
+def stack_days(surfaces, targets, stacks):
+
+    dim = len(surfaces.index.get_level_values(0).unique())
+    dim1 = len(surfaces.index.get_level_values(1).unique())
+
+    surfaces_np = surfaces.values.reshape((dim, dim1, surfaces.shape[1]))
+
+    stacked_surfaces = np.zeros((len(surfaces_np)-stacks, dim1, surfaces.shape[1], stacks))
+    
+    for i in range(len(surfaces_np)-stacks):
+        temp = np.stack(surfaces_np[i:i+stacks]).transpose(1,2,0)
+        stacked_surfaces[i] = temp
+
+    stacked_dates = surfaces.index.get_level_values(0).unique()[stacks:]
+    
+    return stacked_surfaces, targets.loc[stacked_dates], stacked_dates
+
+
+def lag_targets(surfaces, targets, lag, dates):
+    lagged = targets.shift(-lag)
+    return surfaces[:-lag], lagged.dropna(), dates[lag:]
+
+
+def train_test_split(test_start, test_end, dates, surfaces, targets):
+    test_dates = pd.Series(dates, index=dates)[test_start:test_end]
+    
+    start_ind = dates.get_loc(test_dates.iloc[0])
+    end_ind = dates.get_loc(test_dates.iloc[-1]) + 1
+        
+    surfaces_test = surfaces[start_ind:end_ind]
+    targets_test = targets.to_numpy()[start_ind:end_ind]
+    
+    surfaces_train = surfaces[:start_ind]
+    surfaces_train = np.concatenate((surfaces_train,surfaces[end_ind:]))
+
+    targets_train = targets.to_numpy()[:start_ind]
+    targets_train = np.concatenate((targets_train,targets.to_numpy()[end_ind:]))
+    
+    train_dates = pd.Series(dates, index=dates).loc[~pd.Series(dates, index=dates).index.isin(test_dates)].index
+    
+    return surfaces_train, surfaces_test, targets_train, targets_test, train_dates, test_dates
+    
+    
+def r_square(y_true, y_pred):
+    from keras import backend as K
+    SS_res =  K.sum(K.square(y_true - y_pred)) 
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+
+
+
+
